39b08c77ef7f7ca6719d6ca0b0f4ef76462f261c
pallawi342 <pallawikumari62@gmail.com>
2024-05-02 21:05:30 +0530

    work

---
+++ Advance Level Problems/reversenum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        cout<<i<<"  ";
+    }
+    return 0;
+}
